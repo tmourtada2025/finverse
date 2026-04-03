8014bf33c51a54ab4af5d3c4132c44520d98a1f5
tmourtada2025 <tmourtada@gmail.com>
2026-04-03 17:08:06 +0300

    Integrate AuthProvider into App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,23 +13,44 @@ import Resources from "./pages/Resources";
 import Blueprint from "./pages/Blueprint";
 import Education from "./pages/Education";
 import Layout from "./components/Layout";
+import { AuthProvider } from "@/lib/auth";
+import Login from "@/pages/Login";
+import AuthCallback from "@/pages/AuthCallback";
+import Dashboard from "@/pages/Dashboard";
+import CoursePlayer from "@/pages/CoursePlayer";
+import Admin from "@/pages/Admin";
 
 function Router() {
   return (
-    <Layout>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/framework" component={Framework} />
-        <Route path="/blog" component={Blog} />
-        <Route path="/blog/:slug" component={Article} />
-        <Route path="/about" component={About} />
-        <Route path="/resources" component={Resources} />
-        <Route path="/blueprint" component={Blueprint} />
-        <Route path="/education" component={Education} />
-        <Route path="/404" component={NotFound} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
+    <Switch>
+      {/* ── LMS routes (no Layout wrapper) ── */}
+      <Route path="/login" component={Login} />
+      <Route path="/auth/callback" component={AuthCallback} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/learn/:courseId" component={CoursePlayer} />
+      <Route path="/learn/:courseId/:lessonId" component={CoursePlayer} />
+      <Route path="/admin" component={Admin} />
+
+      {/* ── Public site routes (with Layout) ── */}
+      <Route>
+        {() => (
+          <Layout>
+            <Switch>
+              <Route path="/" component={Home} />
+              <Route path="/framework" component={Framework} />
+              <Route path="/blog" component={Blog} />
+              <Route path="/blog/:slug" component={Article} />
+              <Route path="/about" component={About} />
+              <Route path="/resources" component={Resources} />
+              <Route path="/blueprint" component={Blueprint} />
+              <Route path="/education" component={Education} />
+              <Route path="/404" component={NotFound} />
+              <Route component={NotFound} />
+            </Switch>
+          </Layout>
+        )}
+      </Route>
+    </Switch>
   );
 }
 
@@ -38,8 +59,10 @@ function App() {
     <ErrorBoundary>
       <ThemeProvider defaultTheme="dark">
         <TooltipProvider>
-          <Toaster />
-          <Router />
+          <AuthProvider>
+            <Toaster />
+            <Router />
+          </AuthProvider>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
