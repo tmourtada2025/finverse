0919e34fc12b0f5276745c4e9df0e8773d8b718c
tmourtada2025 <tmourtada@gmail.com>
2026-05-10 08:11:12 +0300

    Add files via upload

--- client/src/pages/CourseLandingSMC.tsx
+++ client/src/pages/CourseLandingSMC.tsx
@@ -9,7 +9,7 @@ import { ArrowRight, CheckCircle, BarChart2, Layers, Compass, Target, Activity, 
 import { supabase } from "@/lib/supabase";
 import SEO, { courseSchema } from "@/components/SEO";
 
-const STRIPE_LINK = "REPLACE_WITH_STRIPE_PAYMENT_LINK"; // TODO: paste Stripe Payment Link URL once created
+const STRIPE_LINK = "https://buy.stripe.com/aFa4gzdaG0zY6pb4p2fjG04";
 const COURSE_SLUG = "smc-complete-guide";
 const INTRO_VIDEO_URL = "https://iframe.mediadelivery.net/embed/656804/12da8be6-7e14-436b-a72d-db16e9d2caf8";
 
